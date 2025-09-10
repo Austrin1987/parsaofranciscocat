f988cdb1704bc5f83d3d57002c2b8768fe97d68d
Austrin1987 <rafael.clopes3@senacsp.edu.br>
2025-09-10 08:36:09 -0300

    Update events.js

--- js/events.js
+++ js/events.js
@@ -391,6 +391,7 @@ function createEventsFilters() {
                 
                 <select id="year-filter" class="filter-select">
                     <option value="all">Todos os anos</option>
+                    <option value="2025">2025</option>
                     <option value="2024">2024</option>
                     <option value="2023">2023</option>
                     <option value="2022">2022</option>
