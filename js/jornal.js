5654ec973758f2eb1a28d626fe329cbc4226c2f7
Austrin1987 <rafael.clopes3@senacsp.edu.br>
2025-10-29 15:35:45 -0300

    Update jornal.js

--- js/jornal.js
+++ js/jornal.js
@@ -542,13 +542,11 @@ document.addEventListener('DOMContentLoaded', () => {
         // 2. Cria container temporário invisível no DOM
         const containerPDF = document.createElement('div');
         containerPDF.id = 'print-content';
-        containerPDF.style.position = 'fixed';
+        containerPDF.style.position = 'absolute';
         containerPDF.style.top = '0';
         containerPDF.style.left = '0';
-        containerPDF.style.width = '100vw';
-        containerPDF.style.height = '100vh';
-        containerPDF.style.opacity = '0';
-        containerPDF.style.pointerEvents = 'none';
+        containerPDF.style.width = '100%';
+        containerPDF.style.visibility = 'hidden';
         containerPDF.style.zIndex = '-1';
         containerPDF.innerHTML = htmlContent;
         document.body.appendChild(containerPDF);
